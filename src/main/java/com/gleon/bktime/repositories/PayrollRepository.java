295df375606c5cee30148c537c63a4f187092807
gonzalo <gleonrivas@safareyes.es>
2023-01-05 04:42:48 +0100

    PAYROLL REPO-SERVICE CREATED

---
+++ src/main/java/com/gleon/bktime/repositories/PayrollRepository.java
@@ -0,0 +1,8 @@
+package com.gleon.bktime.repositories;
+
+import com.gleon.bktime.models.Payroll;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PayrollRepository extends JpaRepository<Payroll, Integer>{
+
+}
