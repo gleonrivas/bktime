28ae55d622e1b826fadb3846eedde907c71f34e5
gonzalo <gleonrivas@safareyes.es>
2023-01-09 06:09:52 +0100

    cleaning

--- src/main/java/com/gleon/bktime/BktimeApplication.java
+++ src/main/java/com/gleon/bktime/BktimeApplication.java
@@ -2,6 +2,8 @@ package com.gleon.bktime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class BktimeApplication {
@@ -9,5 +11,4 @@ public class BktimeApplication {
     public static void main(String[] args) {
         SpringApplication.run(BktimeApplication.class, args);
     }
-
 }
