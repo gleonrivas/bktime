71751f6c916ad4a1f481c4cbc48d9bb5419c656b
gonzalo <gleonrivas@safareyes.es>
2023-01-10 22:17:49 +0100

    PROFILE CHANGE ALMOST

--- src/main/java/com/gleon/bktime/controllers/ProfileController.java
+++ src/main/java/com/gleon/bktime/controllers/ProfileController.java
@@ -29,7 +29,7 @@ public class ProfileController {
 
 
     @GetMapping("/profile")
-    public String requestProfile(Model model){
+    public String requestProfilePage(Model model){
         Integer userId = userRepository.verifyUsernameOrEmail(SecurityContextHolder.getContext().getAuthentication().getName()).getId();
         User user = userRepository.findUserById(userId);
         model.addAttribute("user", user);
@@ -37,8 +37,88 @@ public class ProfileController {
     }
 
 
+    @PostMapping("/profile")
+    public String modifyUser(
+            //@RequestParam(required = false) String userImg,
+            @RequestParam(required = false) String userName,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String surname,
+            //@RequestParam(required = false) String phone,
+            @RequestParam(required = false) String email) {
+
+        Integer userId = userRepository.verifyUsernameOrEmail(SecurityContextHolder.getContext().getAuthentication().getName()).getId();
+        User user = userRepository.findUserById(userId);
+
+        if (userRepository.findUserName(userName) != null && userRepository.findUserById(userId).getUserName().equals(userName)){
+            user.setUserName(userRepository.findUserById(userId).getUserName());
+        } else if (userRepository.findUserName(userName) == null){
+            user.setUserName(userName);
+        } else if (userName == null || userName.equals("")) {
+            return "redirect:/profile?usernameEmpty";
+        }
+
+        if (name == null || name.equals("")) {
+            return "redirect:/profile?nameEmpty";
+        }
+        if (surname == null || surname.equals("")) {
+            return "redirect:/profile?surnameEmpty";
+        }
+
+        if (userRepository.findEmail(email) != null && userRepository.findUserById(userId).getEmail().equals(email)){
+            user.setEmail(userRepository.findUserById(userId).getEmail());
+        } else if (userRepository.findEmail(email) == null){
+            user.setEmail(email);
+        } else if (email == null || email.equals("")) {
+            return "redirect:/profile?emailEmpty";
+        }
+
+        //if (userImg != null) user.setUserImg(userImg);
+
+        /*if (userName == null || userName.equals("")) {
+            return "redirect:/profile?usernameEmpty";
+
+        } else if (userRepository.findUserName(userName) != null) {
+            user.setUserName(userName);
+
+        } else {
+            return "redirect:/profile?usernameInUse";
+        }
+
+        if (name == null || name.equals("")) {
+            return "redirect:/profile?nameEmpty";
+        } else {
+            user.setName(name);
+        }
+
+        if (surname == null || surname.equals("")) {
+            return "redirect:/profile?surnameEmpty";
+        } else {
+            user.setSurname(surname);
+        }
+
+
+        //user.setPhone(phone);
+
+
+        if (email == null || email.equals("")) {
+            return "redirect:/profile?emailEmpty";
+
+        } else if (userRepository.findEmail(email) != null) {
+            return "redirect:/profile?emailInUse";
+
+        } else if (userRepository.findEmail(email).equals(user.getEmail())) {
+            user.setEmail(email);
+
+        } else {
+            user.setEmail(email);
+
+        }*/
+        userService.saveUser(user);
+        return "redirect:/profile?success";
+    }
+
     @GetMapping("/password")
-    public String requestPassword(){
+    public String requestPasswordPage(){
         return "/password.html";
     }
 
@@ -63,5 +143,7 @@ public class ProfileController {
         }
 
     }
+
+
 
 }
